dd2fc4dc2681a5a569ec45c52d8c57290c75dcb3
Markus Nettesheim <markusne@rocketmail.com>
2022-11-15 15:51:17 +0100

    wow

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+require 'faker'
+
+10.times do
+  article = Article.new(
+    title: Faker::Games::Zelda.character,
+    content: Faker::Games::Zelda.item
+  )
+  article.save
+  puts article
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
